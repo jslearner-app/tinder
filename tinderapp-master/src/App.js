5e923a20404ec16a57498292090b65d5502c0679
jslearner-app <primatekoop85@gmail.com>
2020-11-11 15:29:15 +0100

    first commit

---
+++ tinderapp-master/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import TinderCards  from "./TinderCards";
+import SwipeButtons  from "./SwipeButtons";
+import './App.css';
+
+function App() {
+  return (
+    // BEM class naming convention
+    <div className="app">
+      <Header />
+      <TinderCards />
+      <SwipeButtons />
+    </div>
+  );
+}
+
+export default App;
